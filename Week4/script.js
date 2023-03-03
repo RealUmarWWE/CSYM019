8cf4613dbdffccd342b97e32b88e0bf4a4d505e3
RealUmarWWE <m.umarmukhtar1998@gmail.com>
2023-03-03 17:34:39 +0000

    My Load Function EXE4

--- Week4/script.js
+++ Week4/script.js
@@ -9,3 +9,11 @@ function print5()
 print5();
 print5();
 var element = document.getElementById('pageheading');
+
+function myLoadFunction() {
+    var element = document.getElementById('pageheading');
+    element.firstChild.nodeValue = 'New Heading';
+    var element = document.getElementById('paragraph');
+    element.firstChild.nodeValue = 'New paragraph text';
+   }
+   document.addEventListener('DOMContentLoaded', myLoadFunction);
